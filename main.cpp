4bce33e88bfd14ac4bfac7d6d29af0e7673d5c2a
s1140342-cmd <s1140342@dmhs.kh.edu.tw>
2025-10-27 16:26:41 +0800

    name

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
-
-int main() {
-    std::cout<< "hello，world!， world!" << std::endl;
-    return 0;
+int main() { 
+    int age = 16; double height = 172; 
+    std::string name = "Lawrence"; bool is_student = true; 
+    std::cout << "Lawrence " << name << std::endl; 
+    std::cout << "172 " << height << std::endl; 
+    std::cout << "16 " << age << std::endl;
+    std::cout << "true " << is_student << std::endl;
+return 0; 
+    
 }
